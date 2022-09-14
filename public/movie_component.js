5c099f52f4ee495f34f7bbd96ae801fa1821b55f
Fermin <feeralvarez013@gmail.com>
2022-09-14 13:25:48 -0300

    Mejoras en el diseño

--- public/movie_component.js
+++ public/movie_component.js
@@ -8,41 +8,28 @@ function movie_card (image_link, title, year, plot, metacritic_rating, tomatoes_
     tomatoes_rating = tomatoes_rating == null ? ' Desconocido ' : tomatoes_rating
     imdb_rating = imdb_rating == null ? ' Desconocido ' : imdb_rating
 return `<figure class="movie">
-    <div class="movie__hero">
+    <div class="movie__price">
         <img src="` + image_link + `" alt="` + title + `" class="movie__img"></img></div>
         <div class="movie__content">
             <div class="movie__title">
                 <h1 class="heading__primary">` + title + ` ( ` + year + `)
                     <i class="fas fa-fire"></i>
                 </h1>
-                </div>
+            </div>
                 <p class="movie__description">
                 ` + plot + `
                 </p>
                 <div class="movie__details">
                     <p class="movie__detail">
                         <span class="Rating">
-                         METACRITIC: ` + metacritic_rating  + ` 
-                        </span>
-                    </p>
-                </div>
-                <div class="movie__details">
-                    <p class="movie__detail">
-                        <span class="Rating">
                          Tomatoes: ` + tomatoes_rating + ` 
-                        </span>
-                    
-                    </p>
-                </div>
-                <div class="movie__details">
-                    <p class="movie__detail">
-                        <span class="Rating">
-                         IMDB: ` + imdb_rating + ` 
+                         - IMDB: ` + imdb_rating + ` 
+                         - METACRITIC: ` + metacritic_rating  + ` 
                         </span>
                     </p>
                 </div>
-                </div>
-            </div>
-        </figure>
+        </div>
+    </div>
+</figure>
 `
 }
